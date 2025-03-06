f62a59813d5434183a7660cac1411a884831373f
Ariel Barkan <arielbarkan@gmail.com>
2025-03-06 19:44:34 +0200

    first commit

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello, Express with TypeScript!");
+    res.send("Hello, Express with TypeScript and RabbitMQ");
 });
 
 app.listen(PORT, () => {
